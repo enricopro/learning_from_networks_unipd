0a1faa4c7fe2fe32ea3020c8e059eac7ec01ffbb
gp-1108 <pietrogirotto00@gmail.com>
2024-01-21 20:06:23 +0100

    now printing top 5 proteins per metric

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import networkx as nx
 import numpy as np
-from tabulate import tabulate
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import argparse
 
@@ -14,10 +13,11 @@ def read_graph_from_file(file_path):
 
 def calculate_metrics(graph):
     print("Calculating metrics...")
-    avg_closeness_centrality = np.mean(list(nx.closeness_centrality(graph).values()))
-    avg_betweenness_centrality = np.mean(list(nx.betweenness_centrality(graph).values()))
-    global_clustering_coefficient = nx.average_clustering(graph)
-    return avg_closeness_centrality, avg_betweenness_centrality, global_clustering_coefficient
+    closeness_centralities = nx.closeness_centrality(graph)
+    betweenness_centralities = nx.betweenness_centrality(graph)
+    clustering_coefficients = nx.clustering(graph)
+
+    return closeness_centralities, betweenness_centralities, clustering_coefficients
 
 def calculate_z_scores(graph, random_graphs):
     print("Calculating z-scores...")
@@ -46,21 +46,52 @@ def generate_random_graphs(graph, num_graphs=10):
     return random_graphs
 
 def save_results_to_file(results, output_file_path):
-    print("\nResults Table:")
-    print(tabulate(results, headers="keys", tablefmt="grid"))  # Change "grid" to "pipe" or "plain" for different styles
+    # result = {
+    #     'File Name': os.path.basename(file_path),
+    #     'Avg Closeness': avg_closeness,
+    #     'Avg Betweenness': avg_betweenness,
+    #     'Global Clustering': global_clustering,
+    #     'Z-scores - Closeness': closeness_z,
+    #     'Z-scores - Betweenness': betweenness_z,
+    #     'Z-scores - Clustering': clustering_z,
+    #     'Top 5 Closeness': top_5_closeness,
+    #     'Top 5 Betweenness': top_5_betweenness,
+    #     'Top 5 Clustering': top_5_clustering
+    # }
+    print(f"Saving results to file: {output_file_path}")
+    with open(output_file_path, 'w') as f:
+        f.write("File name: " + results['File Name'] + "\n")
+        f.write("Avg Closeness: " + str(results['Avg Closeness']) + "\n")
+        f.write("Avg Betweenness: " + str(results['Avg Betweenness']) + "\n")
+        f.write("Global Clustering: " + str(results['Global Clustering']) + "\n")
+        f.write("Z-scores - Closeness: " + str(results['Z-scores - Closeness']) + "\n")
+        f.write("Z-scores - Betweenness: " + str(results['Z-scores - Betweenness']) + "\n")
+        f.write("Z-scores - Clustering: " + str(results['Z-scores - Clustering']) + "\n")
+        f.write("Top 5 Closeness: \n")
+        for protein in results['Top 5 Closeness']:
+            f.write("\t" + str(protein) + "\n")
+        f.write("Top 5 Betweenness: \n")
+        for protein in results['Top 5 Betweenness']:
+            f.write("\t" + str(protein) + "\n")
 
-    with open(output_file_path, "a") as file:
-        file.write(tabulate(results, headers="keys", tablefmt="pipe"))
-        file.write("\n\n")
 
 def analyze_file(file_path, output_file_path):
     graph = read_graph_from_file(file_path)
-    avg_closeness, avg_betweenness, global_clustering = calculate_metrics(graph)
+    closeness_centralities, betweenness_centralities, clustering_coefficients = calculate_metrics(graph)
+
+    # Calculate average closeness and betweenness
+    avg_closeness = np.mean(list(closeness_centralities.values()))
+    avg_betweenness = np.mean(list(betweenness_centralities.values()))
+    global_clustering = np.mean(list(clustering_coefficients.values()))
 
     # Generate 10 random graphs for each graph
     random_graphs = generate_random_graphs(graph, num_graphs=10)
 
     closeness_z, betweenness_z, clustering_z = calculate_z_scores(graph, random_graphs)
+
+    # Extracting top 5 proteins per metric
+    top_5_closeness = sorted(closeness_centralities.items(), key=lambda item: item[1], reverse=True)[:5]
+    top_5_betweenness = sorted(betweenness_centralities.items(), key=lambda item: item[1], reverse=True)[:5]
 
     result = {
         'File Name': os.path.basename(file_path),
@@ -69,18 +100,19 @@ def analyze_file(file_path, output_file_path):
         'Global Clustering': global_clustering,
         'Z-scores - Closeness': closeness_z,
         'Z-scores - Betweenness': betweenness_z,
-        'Z-scores - Clustering': clustering_z
+        'Z-scores - Clustering': clustering_z,
+        'Top 5 Closeness': top_5_closeness,
+        'Top 5 Betweenness': top_5_betweenness,
     }
 
-    save_results_to_file([result], output_file_path)
+    save_results_to_file(result, output_file_path)
 
-def main(folder_path, output_file):
-
-    if os.path.exists(output_file):
-        os.remove(output_file)  # Remove existing results file
+def main(folder_path, output_folder_path):
+    if not os.path.exists(output_folder_path):
+        os.makedirs(output_folder_path)
 
     with ProcessPoolExecutor() as executor:
-        futures = [executor.submit(analyze_file, os.path.join(folder_path, file_name), output_file) for file_name in os.listdir(folder_path)]
+        futures = [executor.submit(analyze_file, os.path.join(folder_path, file_name), os.path.join(output_folder_path,file_name)) for file_name in os.listdir(folder_path)]
 
         for future in as_completed(futures):
             try:
@@ -91,7 +123,7 @@ def main(folder_path, output_file):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process a folder of graphs")
     parser.add_argument("-f", "--folder", help="Path to folder containing graphs")
-    parser.add_argument("-o", "--output", help="Path to output file")
+    parser.add_argument("-o", "--output", help="Path to output folder")
     args = parser.parse_args()
 
     main(args.folder, args.output)
